bc478b7b621fe1dcdb45851a4ff468f76401aafe
Fajar Antono <fajar.antono988@gmail.com>
2022-01-02 22:42:41 +0700

    database: added database seeder

--- app/server.go
+++ app/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/fajarantono/ecomerce-go/database/seeders"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -38,6 +39,7 @@ func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
 
 	server.InitializeDB(dbConfig)
 	server.InitializeRoutes()
+	seeders.DBSeed(server.DB)
 }
 
 func (server *Server) Run(addr string) {
